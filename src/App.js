acae5b88d6c469dca73ba13d62daf0b72e2db9ff
gitkonst <34488160+gitkonst@users.noreply.github.com>
2018-01-17 01:36:52 +0300

    Created ReplyUpvotesMore component.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
-import postJSON from './sample_reddit_json_14.01.18';
-import './App.css';
+import ReplyUpvotesMore from './ReplyUpvotesMore';
 
+import submissionJSON from './sample_reddit_json_14.01.18';
 
 class App extends Component {
   render() {
-    let gif = postJSON.preview.images[0].variants.gif.source;
+    let gif = submissionJSON.preview.images[0].variants.gif.source;
     return (
-      <div className="App text-muted">
-        <div style={{margin: "10px"}}>
-          <div className="text-primary">{postJSON.subreddit_name_prefixed}</div>
-          <h4>{postJSON.title}</h4>
-          <div>u/{postJSON.author}</div>
+      <div>
+        <div className="container-fluid text-muted">
+          <p className="text-primary my-1">{submissionJSON.subreddit_name_prefixed}</p>
+          <h4><small>{submissionJSON.title}</small></h4>
+          <p className="my-2">u/{submissionJSON.author}</p>
         </div>
-        <img src={gif.url} style={{width: "100%", maxWidth: gif.width}}/>
-        <div>{postJSON.num_comments} Comments</div>
+        <img
+          src={gif.url}
+          className="img-fluid d-block"
+          alt={submissionJSON.title}
+        />
+        <ReplyUpvotesMore
+          num_upvotes={submissionJSON.ups}
+        />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import 'font-awesome/css/font-awesome.css'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './include/bootstrap'
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+
