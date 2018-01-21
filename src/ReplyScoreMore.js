9102346f7d2d65e51bd9278919caed15f8326aa1
gitkonst <34488160+gitkonst@users.noreply.github.com>
2018-01-21 16:54:10 +0300

    Added reply icon

--- src/ReplyScoreMore.js
+++ src/ReplyScoreMore.js
@@ -4,6 +4,9 @@ function ReplyScoreMore({score}) {
   return (
     <div className="clearfix">
       <ul className="list-inline container-fluid text-very-muted float-right float-md-left py-2 mb-0">
+        <li className="list-inline-item pl-0 pr-3">
+          <i class="fa fa-reply" aria-hidden="true" role="button"></i>
+        </li>
         <li className="list-inline-item px-1">
           <i className="fa fa-ellipsis-h" aria-hidden="true" role="button"></i>
         </li>
